f277b267ea1ff3fbc29dcfc927567b2a4096e27a
piotr.zadora <piotr.zadora@ASSET-13.local>
2022-09-26 11:34:12 +0200

    Initial commit

---
+++ src/main/java/com/example/jenkinsheroku/JenkinsHerokuApplication.java
@@ -0,0 +1,21 @@
+package com.example.jenkinsheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsHerokuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsHerokuApplication.class, args);
+    }
+
+    @GetMapping
+    public String get() {
+        return "piorun";
+    }
+
+}
